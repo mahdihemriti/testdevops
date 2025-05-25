fd1c926e4540dc2c78dcb715e255daeb3870d831
mahdi <mahdi.hemriti@esprit.tn>
2025-05-25 19:29:50 +0100

    add dto and mapper

--- src/main/java/tn/esprit/eventsproject/mapper/EventMapper.java
+++ src/main/java/tn/esprit/eventsproject/mapper/EventMapper.java
@@ -4,6 +4,11 @@ import tn.esprit.eventsproject.dto.EventDTO;
 import tn.esprit.eventsproject.entities.Event;
 
 public class EventMapper {
+    // Empêche l'instanciation de la classe
+    private EventMapper() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
     public static Event toEntity(EventDTO dto) {
         Event e = new Event();
         e.setDescription(dto.getDescription());
--- src/main/java/tn/esprit/eventsproject/mapper/ParticipantMapper.java
+++ src/main/java/tn/esprit/eventsproject/mapper/ParticipantMapper.java
@@ -4,6 +4,11 @@ import tn.esprit.eventsproject.dto.ParticipantDTO;
 import tn.esprit.eventsproject.entities.Participant;
 
 public class ParticipantMapper {
+    // Constructeur privé pour empêcher l’instanciation
+    private ParticipantMapper() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
     public static Participant toEntity(ParticipantDTO dto) {
         Participant p = new Participant();
         p.setNom(dto.getNom());
